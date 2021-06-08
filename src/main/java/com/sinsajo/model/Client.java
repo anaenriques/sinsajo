e45a8296a9d988b156fd3b2d210002024c93f0db
Germán ZM <gzuritam@everis.com>
2021-06-08 17:49:50 +0200

    Serializable Client

--- src/main/java/com/sinsajo/model/Client.java
+++ src/main/java/com/sinsajo/model/Client.java
@@ -1,5 +1,7 @@
 package com.sinsajo.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -20,7 +22,9 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Client {
+public class Client implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
