f6e30c6501449b9905bde1d7742210193970acd2
ecalvach <ecalvach@SVQ-6Y71693.usersad.everis.int>
2021-06-08 12:44:28 +0200

    cambios en el pom y en el application properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO CLIENT (first_name, last_name, age, dni) VALUES
+  ('Katniss', 'Everdeen', 16, '06900025W'),
+  ('Peeta', 'Mellark', 17, '00235146V'),
+  ('Primrose', 'Everdeen', 13, '69662510H');
