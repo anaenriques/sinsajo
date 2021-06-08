0145d00c69ea03e4c2ee68e1e06caa3e2c54a601
ecalvach <ecalvach@SVQ-6Y71693.usersad.everis.int>
2021-06-08 10:31:29 +0200

    subida

---
+++ src/main/java/com/sinsajo/SinsajoApplication.java
@@ -0,0 +1,13 @@
+package com.sinsajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinsajoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinsajoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinsajo/SinsajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinsajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinsajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
