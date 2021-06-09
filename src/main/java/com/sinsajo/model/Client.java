f2532069b00ab4a806b6693e2a7c7fe51075881e
aenriqus <aenriqus@SVQ-CXQ9793.usersad.everis.int>
2021-06-09 10:49:20 +0200

    rama brunch

--- src/main/java/com/sinsajo/model/Client.java
+++ src/main/java/com/sinsajo/model/Client.java
@@ -16,8 +16,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
-import lombok.Value;
+
 
 @Entity
 @Table(name = "CLIENT")
