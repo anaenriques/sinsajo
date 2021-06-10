6735a573c67f84604860c5746246c6ed74fe048b
Germán ZM <gzuritam@everis.com>
2021-06-10 13:52:25 +0200

    Conf H2 Memory

--- src/main/java/com/sinsajo/SinsajoApplication.java
+++ src/main/java/com/sinsajo/SinsajoApplication.java
@@ -1,13 +1,20 @@
 package com.sinsajo;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SinsajoApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SinsajoApplication.class, args);
 	}
-
+	
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseServer() throws Exception {
+		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+	}
+	
 }
